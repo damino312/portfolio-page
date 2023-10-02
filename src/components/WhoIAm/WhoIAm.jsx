be77c9d8111a2a743d2534355ea132447e1bc99d
Popov Kirill <93064817+damino312@users.noreply.github.com>
2023-10-02 08:06:14 +0700

    Additional changes

--- src/components/WhoIAm/WhoIAm.jsx
+++ src/components/WhoIAm/WhoIAm.jsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./WhoIAm.scss";
 
 export default function WhoIAm() {
   return (
     <section className="whoIAm">
-      <div className="wrapper whoIAm__wrapper">
-        <h1 className="whoIAm__left text--gray-white">
-          <span className="whoIAm__text">Hey 👋,</span>
-          <span className="whoIAm__text">My name is</span>
-          <span className="whoIAm__text text-gradient">Kirill</span>
-          <span className="whoIAm__text">And I&apos;m building</span>
-          <span className="whoIAm__text">my carrier in web</span>
-        </h1>
-        <div className="whoIAm__right">
-          <div className="whoIAm__photo-container">
-            <img
-              src="/portfolio-page/my_photo.jpg"
-              alt="Here is supposed to be my photo"
-              className="whoIAm__photo"
-            />
+      <div className="wrapper ">
+        <div className="whoIAm__wrapper">
+          <h1 className="whoIAm__left text--gray-white">
+            <span className="whoIAm__text">Hey 👋,</span>
+            <span className="whoIAm__text">My name is</span>
+            <span className="whoIAm__text text-gradient">Kirill</span>
+            <span className="whoIAm__text">And I&apos;m building</span>
+            <span className="whoIAm__text">my carreer in web</span>
+          </h1>
+          <div className="whoIAm__right">
+            <div className="whoIAm__photo-container">
+              <img
+                src="/portfolio-page/my_photo.jpg"
+                alt="Here is supposed to be my photo"
+                className="whoIAm__photo"
+              />
+            </div>
           </div>
         </div>
+        <motion.a
+          className="whoIAm__cv"
+          target="_blank"
+          href="https://drive.google.com/file/d/1n41_vfWAyep3jNM7XgA9DIECFo6oiNeH/view?usp=sharing"
+          whileHover={{ scale: 1.3, originX: 0 }}
+        >
+          &gt; Download my cv
+        </motion.a>
       </div>
     </section>
   );
